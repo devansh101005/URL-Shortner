95712563d481fc1d0d4a09782c168ead37330a55
Devansh Pandey <devanshpandeyji4321@gmail.com>
2025-09-05 02:38:49 +0530

    change in db

--- models/user.model.js
+++ models/user.model.js
@@ -2,11 +2,15 @@ import { timestamp,text,varchar,pgTable, uuid } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
   id:uuid().primaryKey().defaultRandom(),
+
   firstname: varchar('first_name,{length:55').notNull(),
   lastname: varchar('last_name',{length:55}),
+
   email:varchar({length:255}).notNull().unique(),
+
   salt:text().notNull(),
   password:text().notNull(),
+  
   createdAt:timestamp('created_at').defaultNow().notNull(),
   updatedAt:timestamp('created_at').defaultNow().notNull(),
 
