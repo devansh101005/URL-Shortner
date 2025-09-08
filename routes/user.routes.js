c05ec37c88f2a3103c6af1126928aac8d14e35b0
Devansh Pandey <devanshpandeyji4321@gmail.com>
2025-09-08 09:20:21 +0530

    fixed user routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,28 @@
 import express from 'express';
+import {db} from '../db/index.js';
+import{usersTable} from '../models/index.js';
+import {eq} from 'drizzle-orm';
+import {randomBytes } from 'crypto';
 const router =express.Router();
 
 router.post('/signup',async (req,res) => {
     const { firstname,lastname,email,password} =req.body;
 
+    const [existingUser]=await db 
+    .select({
+        id:usersTable.id,
+    })
+    .from(usersTable)
+    .where(eq(usersTable.email,email))
+
+    if(existingUser)
+        return res.status(400)
+    .json({error:`User with email4{email} already exist`});
+
+    const user =await 
+
     if(!firstname)
         return res.status(400).json({error:'firstname is required'})
 })
+
+export default router;
