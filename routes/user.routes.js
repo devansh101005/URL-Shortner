df80e23113d13e6bfd1daac6262d9bd181652db9
Devansh Pandey <devanshpandeyji4321@gmail.com>
2025-09-12 11:36:18 +0530

    code refactored

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import {db} from '../db/index.js';
 import {usersTable} from '../models/index.js';
-import {eq} from 'drizzle-orm';
-import {randomBytes,createHmac } from 'crypto';
+//import {eq} from 'drizzle-orm';
+//import {randomBytes,createHmac } from 'crypto';
 import {signupPostRequestBodySchema} from '../validation/request.validation.js';
+import {hashPasswordWithSalt} from '../utils/hash.js'
+import {getUserByEmail} from '../services/user.service.js'
 const router =express.Router();
 
 router.post('/signup',async (req,res) => {
@@ -17,21 +19,24 @@ const { firstname,lastname,email,password }=validationResult.data;
 
 
 
-    const [existingUser]=await db 
-    .select({
-        id:usersTable.id,
-    })
-    .from(usersTable)
-    .where(eq(usersTable.email,email))
+    // const [existingUser]=await db 
+    // .select({
+    //     id:usersTable.id,
+    // })
+    // .from(usersTable)
+    // .where(eq(usersTable.email,email))
+
+    const existingUser= await getUserByEmail(email)
 
     if(existingUser)
         return res.status(400)
-    .json({error:`User with email4{email} already exist`});
+    .json({error:`User with email ${email} already exist`});
 
-    const salt=randomBytes(256).toString('hex');
-    const hashedPassword=createHmac('sha256',salt).update(password).digest('hex');
+    //const salt=randomBytes(256).toString('hex');
+    //const hashedPassword=createHmac('sha256',salt).update(password).digest('hex');
 
-
+    
+    const {salt,password:hashedPassword} =hashPasswordWithSalt(password);
 
 
 
